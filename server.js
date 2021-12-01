f9e37935db06812d011d6a1ece4309928e2ba473
bulliss93 <84363525+bulliss93@users.noreply.github.com>
2021-11-30 18:41:03 -0800

    Add files via upload
    barebones server implementation without links to templates

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+var express = require('express');
+var exphbs = require('handlebars-express');
+
+// default port 7000
+var port = process.env.PORT || 7000;
+var app = express();
+
+// establish handlebars dependencies
+app.engine('handlebars', exphbs.engine({defaultLayout: null}));
+app.set('view engine', 'handlebars');
+
+// link public folder
+app.use(express.static('public'));
+
+
+// fill in stuff here
+
+
+
+
+// catch all middleware function
+app.get("*", function (req, res, next) {
+    res.status(404).render('404', {
+        path: req.url
+    })
+});
+
+// listen on port 
+app.listen(port, function(err) {
+    if(err) {
+        throw err;
+    }
+    console.log("Listenning on port", port);
+});
+
+
+
+
+
 
