dd96ab654a81a671e022ea969932140976c05e23
madaniloff <madstagherd@gmail.com>
2021-12-05 14:44:17 -0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
+//Function to insert new links
+function insertNewLink(description, link) {
+    var context = {
+        description: description,
+        link: link
+    }
 
+    var newLink = Handlebars.templates.linkTemplate(context)
+    var linkContainer = document.getElementsByClassName('contentLinks')
+    linkContainer.insertAdjacentHTML('beforeend', newLink)
+}
