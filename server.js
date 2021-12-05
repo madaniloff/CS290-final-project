4728c31c87df7bfe75a5208acd2f34f47409b82c
madaniloff <madstagherd@gmail.com>
2021-12-05 14:26:29 -0800

    Added server functionalities for different categories and added default layout

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 7000;
 var app = express();
 
 // establish handlebars dependencies
-app.engine('handlebars', exphbs.engine({defaultLayout: null}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // link public folder
@@ -15,6 +15,49 @@ app.use(express.static('public'));
 
 // fill in stuff here
 
+//Home page
+app.get("/", function (req, res, next) {
+
+})
+
+app.get("/home", function (req, res, next) {
+
+})
+
+//Locations
+app.get("/locations", function (req, res, next) {
+
+})
+
+//Encounters
+app.get("/encounters", function(req, res, next) {
+
+})
+
+//Items
+app.get("/items", function (req, res, next) {
+
+})
+
+//Characters
+app.get("/characters", function (req, res, next) {
+
+})
+
+//Creatures
+app.get("/creatures", function (req, res, next) {
+
+})
+
+//Classes
+app.get("/classes", function (req, res, next) {
+
+})
+
+//Music
+app.get("/music", function (req, res, next) {
+
+})
 
 
 
