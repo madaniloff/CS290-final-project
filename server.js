a4ee3b259960a4fde79fb87e003a19485865d82a
bulliss93 <84363525+bulliss93@users.noreply.github.com>
2021-12-05 19:37:56 -0800

    Add files via upload
    added functionality for if page doesn't exist and specific searches within a section

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var pageData = require(''); // DOM FILE
+var pageData = require('./postData.json'); // DOM FILE
 
 // NEED TO INCLUDE DOM FILES
 
@@ -15,9 +15,8 @@ app.set('view engine', 'handlebars');
 // link public folder
 app.use(express.static('public'));
 
-var page_type
-
-// fill in stuff here
+var pageType
+var exists = 0
 
 //Home page
 app.get("/", function (req, res, next) {
@@ -32,64 +31,241 @@ app.get("/home", function (req, res, next) {
 
 //Locations
 app.get("/locations", function (req, res, next) {
-    page_type = 'locations';
+    pageType = 'locations';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
+        });
+    next();
+})
+
+//Locations specific
+app.get("/locations/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'locations';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'locations'){
+            exists = 1;
+        }
     });
+    
+    if(exists === 1){
+         res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+
     next();
 })
 
 //Encounters
 app.get("/encounters", function(req, res, next) {
-    page_type = 'encounters';
+    pageType = 'encounters';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
     });
+    next();
+})
+
+//Encounters specific
+app.get("/encounters/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'encounters';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'encounters'){
+            exists = 1;
+        }
+    });
+    if(exists === 1 ){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        }); 
+    }
     next();
 })
 
 //Items
 app.get("/items", function (req, res, next) {
-    page_type = 'items';
+    pageType = 'items';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
     });
+    next();
+})
+
+//items specific
+app.get("/items/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'items';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'items'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
     next();
 })
 
 //Characters
 app.get("/characters", function (req, res, next) {
-    page_type = 'characters';
+    pageType = 'characters';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
     });
+    next();
+})
+
+//characters specific
+app.get("/characters/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'characters';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'characters'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
     next();
 })
 
 //Creatures
 app.get("/creatures", function (req, res, next) {
-    page_type = 'creatures';
+    pageType = 'creatures';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
     });
+    next();
+})
+
+//creatures specific
+app.get("/creatures/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'creatures';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'creatures'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
     next();
 })
 
 //Classes
 app.get("/classes", function (req, res, next) {
-    page_type = 'classes';
+    pageType = 'classes';
     res.status(200).render('contentPage', {
-        type: page_type
+        pageData,
+        pageType
     });
     next();
 })
 
-//Music https://cephanox.bandcamp.com/
-app.get("/music", function (req, res, next) {
-    page_type = 'music';
-    res.status(200).render('contentPage', {
-        type: page_type
+//Classes specific
+app.get("/classes/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'classes';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'classes'){
+            exists = 1;
+        }
     });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+    next();
+})
+
+//Music
+app.get("/music", function (req, res, next) {
+    pageType = 'music';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
+//Music specific
+app.get("/music/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'music';
+    pageData.forEach(function(element){
+        if(element.title === post && element.typ === 'music'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
     next();
 })
 
