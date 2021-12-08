87103be95c3ce3d5c234ac73d8f0744f08e394f6
madaniloff <madstagherd@gmail.com>
2021-12-07 17:08:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,178 @@ app.get("/", function (req, res) {
     });
 })
 
+//Locations specific
+app.get("/locations/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'locations';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'locations'){
+            exists = 1;
+        }
+    });
+
+    if(exists === 1){
+         res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+
+    next();
+})
+
+//Encounters
+app.get("/encounters", function(req, res, next) {
+    pageType = 'encounters';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
+//Encounters specific
+app.get("/encounters/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'encounters';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'encounters'){
+            exists = 1;
+        }
+    });
+    if(exists === 1 ){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        }); 
+    }
+    next();
+})
+
+//Items
+app.get("/items", function (req, res, next) {
+    pageType = 'items';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
+//items specific
+app.get("/items/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'items';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'items'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+    next();
+})
+
+//Characters
+app.get("/characters", function (req, res, next) {
+    pageType = 'characters';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
+//characters specific
+app.get("/characters/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'characters';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'characters'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+    next();
+})
+
+//Creatures
+app.get("/creatures", function (req, res, next) {
+    pageType = 'creatures';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
+//creatures specific
+app.get("/creatures/:post", function (req, res, next) {
+    var post = req.params.post.toLowerCase();
+    pageType = 'creatures';
+    pageData.forEach(function(element){
+        if(element.title === post && element.type === 'creatures'){
+            exists = 1;
+        }
+    });
+    if(exists === 1){
+        res.status(200).render('contentPage', {
+            pageData, 
+            post,
+            pageType
+        });
+    }
+    else{
+        res.status(404).render('404', {
+            path: req.url
+        });
+    }
+    next();
+})
+
+//Classes
+app.get("/classes", function (req, res, next) {
+    pageType = 'classes';
+    res.status(200).render('contentPage', {
+        pageData,
+        pageType
+    });
+    next();
+})
+
 //Classes specific
 app.get("/classes/:post", function (req, res, next) {
     var post = req.params.post.toLowerCase();
@@ -58,38 +230,6 @@ app.get("/music", function (req, res, next) {
     next();
 })
 
-//Music specific
-app.get("/music/:post", function (req, res, next) {
-    var post = req.params.post.toLowerCase();
-    pageType = 'music';
-    pageData.forEach(function(element){
-        if(element.title === post && element.typ === 'music'){
-            exists = 1;
-        }
-    });
-    if(exists === 1){
-        res.status(200).render('contentPage', {
-            pageData, 
-            post,
-            pageType
-        });
-    }
-    else{
-        res.status(404).render('404', {
-            path: req.url
-        });
-    }
-    next();
-})
-
-
-// catch all middleware function
-app.get("*", function (req, res, next) {
-    res.status(404).render('404', {
-        path: req.url
-    })
-});
-
 // listen on port 
 app.listen(port, function(err) {
     if(err) {
@@ -97,6 +237,13 @@ app.listen(port, function(err) {
     }
     console.log("Listenning on port", port);
 });
+
+
+
+
+
+
+
 
 
 
